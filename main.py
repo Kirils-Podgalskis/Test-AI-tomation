fd4347a519bc3e1ff3dcfef67086f6ec7495f38d
kirilspodgalskis <kirils.podgalskis@loadero.com>
2024-11-03 18:53:19 +0200

    update main

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ from anthropic import Anthropic
 from dotenv import load_dotenv
 from compress_guidelines import compress_guidelines
 from compress_test_cases import compress_test_cases
+from compress_system_info import compress_system_info
+import re
 load_dotenv()
 
 
 API_KEY                     =os.getenv('ANTHROPIC_API_KEY')
 COMPRESSED_GUIDELINES_PATH  =os.getenv('COMPRESSED_GUIDELINES_PATH')
 COMPRESSED_TEST_CASES       =os.getenv('COMPRESSED_TEST_CASES')
+COMPRESSED_SYS_INFO_PATH    =os.getenv('COMPRESSED_SYS_INFO_PATH')
 PATH_TO_WRITE_OUTPUT        =os.getenv('PATH_TO_WRITE_OUTPUT')
+
+TEMPERATURE = float(input("Enter temperature:"))
 
 client = Anthropic(
     api_key=API_KEY,
@@ -27,43 +32,58 @@ if not os.path.exists(COMPRESSED_TEST_CASES):
     compress_test_cases()
 
 with open(COMPRESSED_TEST_CASES, 'r', encoding='utf-8') as file:
-    compress_test_casess = file.read()
+    compressed_test_cases = file.read()
 
-# TODO: provide POMs
-    # TODO: what format shall I provide POMs?n
-poms = ['path/to/pom1.py', 'path/to/pom2.py']
-poms_strings = []
-# for pom in poms:
-    # with open(pom, 'r', encoding='utf-8') as file:
-    #     pom_string = file.read()
-    #     poms_strings.add(pom_string) 
+# Prepare system info
+if not os.path.exists(COMPRESSED_SYS_INFO_PATH):
+    compress_system_info()
 
-# TODO: makes this a list for comparing different temperatures
-TEMPERATURE = 0
-additional_information:str = None
-
-prompt = f"Reconstruct compressed test case in <compressed_test_case>.\n<compressed_test_case>\n{compress_test_casess}\n</compressed_test_case>\nUse reconstructed test case to generate a test script.\nFollow compressed project code guidelines for code generation:\n<compressed_guidelines>\n{compressed_guidelines}\n</compressed_guidelines> \nOutput code only."
-
-if additional_information is not None and additional_information is not '':
-    prompt += 'Consider following additional information when generating code:\n<additional_information>\n'
-    prompt += additional_information
-    prompt += '</additional_information>\n'
-
-if len(poms_strings) is not 0:
-    prompt += 'Refer to following page object models during test generation:\n<page_object_models>\n'
-    for pom_string in poms_strings:
-        prompt += f'{pom_string}\n'
-    prompt += '</page_object_models>\n'
+with open(COMPRESSED_SYS_INFO_PATH, 'r', encoding='utf-8') as file:
+    compressed_system_info = file.read()
 
 
-# TODO: provide modal currently available test cases with pytest --cleat-test,
-#   so it pottentially skip certain test generation?
+# Providing POMS
+pom_files = set()
+while True:
+    paths = input("Enter one or multiple file paths (separated by commas) or press Enter to finish: ")
+    if paths == "":
+        break
+    
+    pom_files.add(paths)
+
+print("Collected file paths:", pom_files)
+
+# convert files to strings
+stringified_poms = []
+for pom in pom_files:
+    with open(pom, 'r', encoding='utf-8') as file:
+        pom_file = file.read()
+        selectors = re.findall("\s{4}([A-Za-z_]+)\s*=\s*\(By",pom_file)
+        page_name = re.findall("class (\w+)", pom_file)
+        stringified_poms.append({page_name[0]:selectors}) 
+
+# add files to query
+page_objects:str = ""
+if len(stringified_poms) > 0:
+    page_objects += 'Refer to following page object models during test generation:\n<page_object_models>\n'
+    for pom_string in stringified_poms:
+        page_objects += f'{pom_string}\n'
+    page_objects += '</page_object_models>\n'
+
+user_provided_information = input("Enter additional information or press Enter to finish: ")
+additional_information:str = ""
+if user_provided_information is not None and user_provided_information != '':
+    additional_information += 'Consider following additional information when generating code:\n<additional_information>\n'
+    additional_information += f'{user_provided_information}\n'
+    additional_information += '</additional_information>\n'
+
+prompt = f"Reconstruct system descryption in <compressed_system_description> and use this information in future tasks.\n<compressed_system_description>\n{compressed_system_info}\n</compressed_system_description>\nReconstruct compressed test case in <compressed_test_case>.\n<compressed_test_case>\n{compressed_test_cases}\n</compressed_test_case>\nUse reconstructed test case to generate a test script. Generate test script code step-by-step: each action and check must be pepresented by code. Consider, that test case is meant for manual testing, therefore test case may not explicitly mention element waits. Carefully analyze each step and 2 neighboring steps to generate robust test and exlude posibilty of the test producing false negatives during execution.\nFollow compressed project code guidelines for code generation:\n<compressed_guidelines>\n{compressed_guidelines}\n</compressed_guidelines>\n{page_objects}{additional_information}\nOutput code only."
 
 message = client.messages.create(
     model="claude-3-5-sonnet-20240620",
-    max_tokens=5000,
+    max_tokens=12000,
     temperature=TEMPERATURE,
-    system="You will be using Python 3.10, pytest v8.3.3, and selenium v4.25.0 to generate automated end-to-end test scripts.",
+    system="You will be using Python 3.10, pytest v8.3.3, and selenium v4.25.0 to generate automated end-to-end test scripts for graphical user interaface test automation.",
     messages=[
         {
             "role": "user",
